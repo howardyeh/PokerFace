cc686d598fc2a4f3c4821f339f579c7a69893e1c
weining840123 <liangweining840123@gmail.com>
2019-12-03 18:24:36 -0800

    modify random flip/bright

--- src/preprocess.py
+++ src/preprocess.py
@@ -27,9 +27,11 @@ class Preprocess:
 		np.random.shuffle(self.all_data)
 		for img, value in self.all_data:
 			img_flip = tf.image.random_flip_left_right(img, seed=1284)
+			img_flip = img_flip.numpy()
 			img_random_crop = self.get_random_crop(img_flip, int(IMG_SIZE * 0.8), int(IMG_SIZE * 0.8))
 			img_random_crop = cv2.resize(img_random_crop, (IMG_SIZE, IMG_SIZE))
 			img_random_bright = tf.image.random_brightness(img_random_crop, max_delta=0.5)
+			img_random_bright = img_random_bright.numpy()
 			self.X.append(img)
 			# self.X.append(img_flip)
 			# self.X.append(img_random_crop)
@@ -105,9 +107,11 @@ class Preprocess:
 
 		for img, value in all_data:
 			img_flip = tf.image.random_flip_left_right(img, seed=1284)
+			img_flip = img_flip.numpy()
 			img_random_crop = self.get_random_crop(img_flip, int(IMG_SIZE * 0.8), int(IMG_SIZE * 0.8))
 			img_random_crop = cv2.resize(img_random_crop, (IMG_SIZE, IMG_SIZE))
 			img_random_bright = tf.image.random_brightness(img_random_crop, max_delta=0.5)
+			img_random_bright = img_random_bright.numpy()
 			self.X.append(img)
 			# self.X.append(img_flip)
 			# self.X.append(img_random_crop)
